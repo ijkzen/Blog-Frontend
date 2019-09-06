38c72dd6464818fd7a3f6ae70260e4033a6aee02
xiongshanghua <xiongshanghua@sz.hitrontech.com>
2019-09-06 12:34:13 +0800

    feat(katex): have a error at code block

--- src/app/showdown/MathExtension.js
+++ src/app/showdown/MathExtension.js
@@ -1,8 +1,15 @@
 "use strict";
 
 const katex = require('katex');
-let codeBlocks = [];
-let codeSearched = false;
+let inlineConfig = {
+  displayMode: false,
+  throwOnError: true
+};
+
+let blockConfig = {
+  displayMode: true,
+  throwOnError: true
+};
 module.exports = function showdownMath() {
   return [
     {
@@ -16,34 +23,14 @@ module.exports = function showdownMath() {
       type: "lang",
       regex: /(¨D¨D)([\s\S]+?)(¨D¨D)/g,
       replace: function (whole, left, content, right, location, text) {
-        let result;
-        if (codeSearched) {
-          result = renderBlock(whole, content);
-        } else {
-          codeSearched = true;
-          searchCodeBlocks(text);
-          result = renderBlock(whole, content);
-        }
-
-        return result;
+        return renderBlock(whole, content);
       }
     },
     {
       type: "lang",
       regex: /(¨D)([\s\S]+?)(¨D)/g,
       replace: function (whole, left, content, right, location, text) {
-        let result;
-        if (codeSearched) {
-          result = renderInline(whole, content);
-          // console.log('searched: ' + result);
-        } else {
-          codeSearched = true;
-          searchCodeBlocks(text);
-          result = renderInline(whole, content);
-          // console.log('not search: ' + result);
-        }
-        // console.log('final result: ' + result);
-        return result;
+        return renderInline(whole, content);
       }
     },
     {
@@ -56,79 +43,12 @@ module.exports = function showdownMath() {
   ];
 };
 
-function searchCodeBlocks(text) {
-  let pattern = /(```)([\s\S]+?)(```)/g;
-  let tmp;
-  while (tmp = pattern.exec(text)) {
-    codeBlocks.push(tmp[0]);
-  }
-  console.log(codeBlocks);
-}
-
 function renderInline(whole, content) {
-  let result;
-  console.log(whole);
-  if (codeBlocks.length === 0) {
-    console.error('count is 0');
-    result = katex.renderToString(
-      content,
-      {
-        displayMode: false,
-        throwOnError: true
-      }
-    );
-  } else {
-    console.error('count is not 0');
-    for (let i = 0, len = codeBlocks.length; i < len; i++) {
-      let index = whole.lastIndexOf("```");
-      if (index === -1) {
-        index = whole.length - 1;
-      }
-      if (codeBlocks[i].includes(whole.substring(0, index))) {
-        result = whole;
-        break;
-      }
-    }
-    if (result !== whole) {
-      result = katex.renderToString(
-        content,
-        {
-          displayMode: false,
-          throwOnError: true
-        }
-      );
-    }
-  }
-  return result;
+  return katex.renderToString(content, inlineConfig);
 }
 
 function renderBlock(whole, content) {
-  let result;
-  if (codeBlocks.length === 0) {
-    result = katex.renderToString(
-      content,
-      {
-        displayMode: true,
-        throwOnError: true
-      }
-    );
-  } else {
-    for (let i = 0, len = codeBlocks.length; i < len; i++) {
-      if (codeBlocks[i].includes(whole)) {
-        result = whole;
-        break;
-      } else {
-        result = katex.renderToString(
-          content,
-          {
-            displayMode: true,
-            throwOnError: true
-          }
-        );
-      }
-    }
-  }
-  return result;
+  return katex.renderToString(content, blockConfig);
 }
 
 
