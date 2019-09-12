808cc92ad135f339c1b8f930bc3feda7be3dfde1
xiongshanghua <xiongshanghua@sz.hitrontech.com>
2019-09-12 14:36:41 +0800

    feat(util): change export location

--- projects/next-showdown/src/index.ts
+++ projects/next-showdown/src/index.ts
@@ -1,7 +1,6 @@
 /*
  * Public API Surface of next-showdown
  */
-export * from './lib/Util/HtmlUtil';
 export * from './lib/service/html.service';
 export * from './lib/service/mermaid.service';
 export * from './lib/next-showdown.module';
--- projects/next-showdown/src/lib/next-showdown.module.ts
+++ projects/next-showdown/src/lib/next-showdown.module.ts
@@ -1,10 +1,14 @@
 import {NgModule} from '@angular/core';
 import {ShowdownComponent} from './showdown/showdown.component';
+import HtmlUtil from './Util/HtmlUtil';
 
 @NgModule({
   declarations: [ShowdownComponent],
   imports: [],
-  exports: [ShowdownComponent]
+  exports: [
+    ShowdownComponent,
+    HtmlUtil,
+  ]
 })
 export class NextShowdownModule {
 }
