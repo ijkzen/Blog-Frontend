7dee76ee22bbd3fa9fb7806b4ee0ac3d31c6a3dc
xiongshanghua <xiongshanghua@sz.hitrontech.com>
2019-11-13 16:01:01 +0800

    tmp commit

--- src/app/admin/report-comment/report-comment.component.ts
+++ src/app/admin/report-comment/report-comment.component.ts
@@ -4,40 +4,79 @@ import {Comment} from '../../service/bean/data/Comment';
 import {NzModalService} from 'ng-zorro-antd';
 
 @Component({
-    selector: 'app-report-comment',
-    templateUrl: './report-comment.component.html',
-    styleUrls: ['./report-comment.component.scss']
+  selector: 'app-report-comment',
+  templateUrl: './report-comment.component.html',
+  styleUrls: ['./report-comment.component.scss']
 })
 export class ReportCommentComponent implements OnInit {
 
-    commentList: Comment[];
+  commentList: Comment[];
 
-    list: number[] = [];
+  commentDisplayList: Comment[] = [];
 
-    constructor(
-        private commentService: CommentService,
-        private modalService: NzModalService
-    ) {
+  list: number[] = [];
+
+  isLoading = true;
+
+  constructor(
+    private commentService: CommentService,
+    private modalService: NzModalService
+  ) {
+  }
+
+  ngOnInit() {
+    this.commentService.getReportedComments()
+      .subscribe(
+        result => {
+          if (result.errCode === '000') {
+            this.commentList = result.list;
+            this.isLoading = false;
+          }
+        }
+      );
+  }
+
+
+  showCommentContent(content: string) {
+    this.modalService.info(
+      {
+        nzTitle: '评论内容',
+        nzContent: content
+      }
+    );
+  }
+
+  handlePageData(event: Comment[]) {
+    this.commentDisplayList = event;
+  }
+
+  selectPage(selected: boolean) {
+    if (selected) {
+      this.list = [];
+      this.commentDisplayList.forEach((value => {
+        this.list.push(value.id);
+      }));
+    } else {
+      this.list = [];
     }
+  }
 
-    ngOnInit() {
-        this.commentService.getReportedComments()
-            .subscribe(
-                result => {
-                    if (result.errCode === '000') {
-                        this.commentList = result.list;
-                    }
-                }
-            );
+  isSelect(id: number): boolean {
+    return this.list.includes(id);
+  }
+
+  selectItem(selected: boolean, id: number) {
+    if (selected) {
+      this.list.push(id);
+    } else {
+      if (this.list.includes(id)) {
+        const index = this.list.findIndex((value => value === id));
+        this.list.splice(index, 1);
+      }
     }
+  }
 
-
-    showCommentContent(content: string) {
-        this.modalService.info(
-            {
-                nzTitle: '评论内容',
-                nzContent: content
-            }
-        );
-    }
+  isIndeterminate(): boolean {
+    return this.list;
+  }
 }
