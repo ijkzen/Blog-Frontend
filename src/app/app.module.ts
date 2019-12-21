8dcd044b72d4c0a37e16257b96ad41bccfe3b035
ijkzen <ijkzen@outlook.com>
2019-12-21 23:14:50 +0800

    feat(site): cancel skeleton animation for loading && package icon static resource

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import {FormsModule} from '@angular/forms';
 import {HtmlService, NextShowdownModule} from 'next-showdown';
 import {MermaidService} from 'next-mermaid';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {NzButtonModule, NzIconModule, NzInputModule, NzSkeletonModule} from 'ng-zorro-antd';
+import {NzButtonModule, NzInputModule, NzSkeletonModule} from 'ng-zorro-antd';
 import {HttpsInterceptor} from './client-interceptor';
 import {ArticleModule} from './article/article.module';
 import {IndexModule} from './index/index.module';
@@ -21,13 +21,53 @@ import {CompareViewComponent} from './edit/compare-view/compare-view.component';
 import {AdminModule} from './admin/admin.module';
 import {OnlyCompareTextComponent} from './admin/only-compare-text/only-compare-text.component';
 import {OnlyCompareViewComponent} from './admin/only-compare-view/only-compare-view.component';
+import {NZ_ICONS, NzIconModule} from 'ng-zorro-antd/icon';
+import {
+  AlipayOutline,
+  AppstoreOutline,
+  ArrowLeftOutline,
+  ArrowRightOutline,
+  ArrowUpOutline,
+  CalendarOutline,
+  DownloadOutline,
+  EditOutline,
+  FolderOutline,
+  HomeOutline,
+  PlayCircleOutline,
+  SearchOutline,
+  SmileOutline,
+  UploadOutline,
+  UserOutline,
+  WechatOutline
+} from '@ant-design/icons-angular/icons';
+import {IconDefinition} from '@ant-design/icons-angular';
+
+
+const icons: IconDefinition[] = [
+  UploadOutline,
+  CalendarOutline,
+  UserOutline,
+  FolderOutline,
+  AlipayOutline,
+  WechatOutline,
+  ArrowUpOutline,
+  DownloadOutline,
+  EditOutline,
+  ArrowRightOutline,
+  ArrowLeftOutline,
+  HomeOutline,
+  AppstoreOutline,
+  SmileOutline,
+  PlayCircleOutline,
+  SearchOutline
+];
 
 @NgModule({
   declarations: [
     AppComponent,
   ],
   imports: [
-    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    BrowserModule.withServerTransition({appId: 'serverApp'}),
     BrowserAnimationsModule,
     HttpClientModule,
     FormsModule,
@@ -48,6 +88,7 @@ import {OnlyCompareViewComponent} from './admin/only-compare-view/only-compare-v
     HtmlService,
     MermaidService,
     {provide: HTTP_INTERCEPTORS, useClass: HttpsInterceptor, multi: true},
+    {provide: NZ_ICONS, useValue: icons}
   ],
   bootstrap: [AppComponent],
   entryComponents: [
