9a136f555a4bba5608ff718049827a27ba4d5852
ijkzen <ijkzen@outlook.com>
2020-02-10 18:38:01 +0800

    fix(build): mock document, window, localstorage

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,13 +1,22 @@
 import {enableProdMode} from '@angular/core';
+import 'localstorage-polyfill';
 
 import {environment} from './environments/environment';
 
 if (environment.production) {
-  enableProdMode();
+    enableProdMode();
 }
 
-export { AppServerModule } from './app/app.server.module';
-export { ngExpressEngine } from '@nguniversal/express-engine';
+// mock
+const MockBrowser = require('mock-browser').mocks.MockBrowser;
+const mock = new MockBrowser();
+global['window'] = mock.getWindow();
+global['document'] = mock.getDocument();
+global['localStorage'] = localStorage;
 
 
-export { renderModule, renderModuleFactory } from '@angular/platform-server';
+export {AppServerModule} from './app/app.server.module';
+export {ngExpressEngine} from '@nguniversal/express-engine';
+
+
+export {renderModule, renderModuleFactory} from '@angular/platform-server';
